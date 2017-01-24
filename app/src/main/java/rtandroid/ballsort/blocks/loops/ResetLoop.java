57365983aa95fcc819dbeb19d74721957cb41cea
Stefan Schake <schake@embedded.rwth-aachen.de>
2017-01-24 18:06:40 +0100

    Updates for RWTH transparent

--- app/src/main/java/rtandroid/ballsort/blocks/loops/ResetLoop.java
+++ app/src/main/java/rtandroid/ballsort/blocks/loops/ResetLoop.java
@@ -22,6 +22,7 @@ import rtandroid.ballsort.blocks.SlingshotMotor;
 import rtandroid.ballsort.blocks.color.ColorPattern;
 import rtandroid.ballsort.blocks.color.ColorType;
 import rtandroid.ballsort.hardware.Sorter;
+import rtandroid.ballsort.hardware.pins.TimedGPIOPin;
 import rtandroid.ballsort.settings.Constants;
 import rtandroid.ballsort.settings.DataState;
 import rtandroid.ballsort.settings.SettingsManager;
@@ -31,6 +32,8 @@ public class ResetLoop extends AStateBlock
     private SlingshotValve mSlingshotValve = null;
     private SlingshotMotor mSlingshotMotor = null;
     private ColorPattern mColorRows = null;
+    private TimedGPIOPin mDropPin = null;
+
 
     public ResetLoop()
     {
@@ -39,6 +42,7 @@ public class ResetLoop extends AStateBlock
         mSlingshotValve = new SlingshotValve();
         mSlingshotMotor = new SlingshotMotor();
         mColorRows = new ColorPattern();
+        mDropPin = new TimedGPIOPin("Feeder", Constants.FEEDER_DROP_PIN, true, false);
     }
 
     @Override
@@ -57,6 +61,7 @@ public class ResetLoop extends AStateBlock
         mSlingshotMotor.start();
 
         // open the pattern
+        mDropPin.setValue(false);
         mColorRows.resetPattern();
     }
 
